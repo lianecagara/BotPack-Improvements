6bb1a0cb112988492fa55cd3b6d45f6db7378469
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-06-06 05:02:44 +0000

    ...

--- modules/commands/chesca.js
+++ modules/commands/chesca.js
@@ -1,3 +1,5 @@
+/* Make sure to have Botpack 1.7.6 before installing! */
+
 module.exports.config = {
   name: "chesca",
   version: "1.0.0",
@@ -11,7 +13,14 @@ module.exports.config = {
   cooldowns: 5,
 };
 
-module.exports.run = async function ({ box, args }) {
+module.exports.run = async function ({ box, args, api, event }) {
+  if (!box || !box?.fetch) {
+    return api.sendMessage(
+      "Unsupported Version, please update your botpack.",
+      event.threadID,
+      event.messageID,
+    );
+  }
   box.lianeAPI("chesca1", "LianeAPI_Reworks", args.join(" "), {
     noEdit: false,
     key: "raw",
